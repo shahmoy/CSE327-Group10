cd30381e5fb7ed766f2f0f7d12ddd0d3fd50b4f5
anika223 <80276911+anika223@users.noreply.github.com>
2021-05-07 20:41:55 +0600

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.eshopp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.rey.material.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button joinnowButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinnowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent.intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
